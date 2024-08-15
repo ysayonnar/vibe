a2a19f21fa277f8c908e54d785463ab62fccda60
ysayonnar <ysayonnar@gmail.com>
2024-08-15 18:18:55 +0300

    styled page for place

--- client/src/components/Place/Place.jsx
+++ client/src/components/Place/Place.jsx
@@ -1,6 +1,7 @@
 import cl from './Place.module.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import ratingStar from '../../static/ratingStar.png'
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
 
 const Place = ({ id }) => {
@@ -22,28 +23,39 @@ const Place = ({ id }) => {
 		getPlaceById()
 	}, [])
 
+	console.log(place)
+
 	return (
 		<div className={cl.main}>
 			{error && <h1 className={cl.error}>{error}</h1>}
 			<div className={cl.mapImage}>
 				<img src={place && `http://localhost:5000/${place.image}`} alt='' />
+				{/* <YMaps>
+					<div className={cl['map__container']}>
+						<Map
+							className={cl.map}
+							defaultState={{
+								center: [place.longtitude, place.width],
+								zoom: 12,
+							}}
+						>
+							<Placemark
+								geometry={[place.longtitude, place.width]}
+								options={{ iconColor: 'red' }}
+							/>
+						</Map>
+					</div>
+				</YMaps> сделать просмотр в модальном окне по нажатию кнопки view on the map */}
 			</div>
-			<YMaps>
-				<div className={cl['map__container']}>
-					<Map
-						className={cl.map}
-						defaultState={{
-							center: [place.longtitude, place.width],
-							zoom: 12,
-						}}
-					>
-						<Placemark
-							geometry={[place.longtitude, place.width]}
-							options={{ iconColor: 'red' }}
-						/>
-					</Map>
+			<div className={cl.info}>
+				<div>
+					<h1 className={cl.name}>{place.name}</h1>
+					<p className={cl.description}>{place.description}</p>
 				</div>
-			</YMaps>
+				<button className={cl.mapButton}>
+					<h1>View on the map</h1>
+				</button>
+			</div>
 		</div>
 	)
 }
